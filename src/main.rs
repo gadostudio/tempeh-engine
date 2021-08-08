5f75a5d4d2f01b7c4f294eeaac08555dca3a8e4d
Andra Antariksa <andra.antariksa@gmail.com>
2021-08-08 12:08:56 +0700

    Whoops

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+#[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen(start)]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 fn main() {
     #[cfg(not(target_os = "android"))]
     env_logger::init();
