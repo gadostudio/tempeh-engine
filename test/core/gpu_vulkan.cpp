73580d20611e3f68da25846b73725afcb51971f4
Muhammad Gusti Nurfathin <gustipart2@gmail.com>
2022-02-11 01:33:14 +0700

    Disable GPU unit test for CI

--- test/core/gpu_vulkan.cpp
+++ test/core/gpu_vulkan.cpp
@@ -7,7 +7,7 @@ using namespace Tempeh;
 #define TEMPEH_EXPECT_OK(x) \
 	EXPECT_EQ(x, GPU::ResultCode::Ok)
 
-TEST(VulkanBackend, GPU)
+TEST(VulkanBackend, Initialization)
 {
 	Log::Logger::init("test");
 	TEMPEH_EXPECT_OK(GPU::Instance::initialize(GPU::BackendType::Vulkan, true));
