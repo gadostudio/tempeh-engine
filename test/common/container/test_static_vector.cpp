c5719b0d70a54fe5c3b723571a39be713f0b1766
Muhammad Gusti Nurfathin <gustipart2@gmail.com>
2022-02-14 09:27:34 +0700

    Forgot to commit the unit test

---
+++ test/common/container/test_static_vector.cpp
@@ -0,0 +1,347 @@
+#include <gtest/gtest.h>
+#include <tempeh/common/typedefs.hpp>
+#include <tempeh/container/static_vector.hpp>
+
+struct TestClassPOD
+{
+	int x;
+	int y;
+	u8 z;
+};
+
+struct TestClass
+{
+	int x;
+	int y;
+	std::string str = "Hello";
+};
+
+TEST(Container_StaticArray, ConstructEmpty)
+{
+	{
+		std::vector<int> vi;
+		Tempeh::Container::StaticVector<int, 4> v;
+		EXPECT_TRUE(v.empty());
+	}
+
+	{
+		Tempeh::Container::StaticVector<TestClassPOD, 4> v;
+		EXPECT_TRUE(v.empty());
+	}
+
+	{
+		Tempeh::Container::StaticVector<TestClass, 4> v;
+		EXPECT_TRUE(v.empty());
+	}
+}
+
+TEST(Container_StaticArray, ConstructDefaultWithCount)
+{
+	{
+		Tempeh::Container::StaticVector<int, 4> v(3);
+
+		EXPECT_TRUE(!v.empty());
+
+		for (const auto& i : v) {
+			EXPECT_EQ(i, 0);
+		}
+	}
+
+	{
+		Tempeh::Container::StaticVector<TestClassPOD, 4> v(3);
+
+		EXPECT_TRUE(!v.empty());
+
+		for (const auto& elem : v) {
+			EXPECT_EQ(elem.x, 0);
+			EXPECT_EQ(elem.y, 0);
+			EXPECT_EQ(elem.z, 0);
+		}
+	}
+
+	{
+		Tempeh::Container::StaticVector<TestClass, 4> v(3);
+
+		EXPECT_TRUE(!v.empty());
+
+		for (const auto& elem : v) {
+			EXPECT_EQ(elem.x, 0);
+			EXPECT_EQ(elem.y, 0);
+			EXPECT_EQ(elem.str, "Hello");
+		}
+	}
+}
+
+TEST(Container_StaticArray, ConstructSplatWithCount)
+{
+	{
+		Tempeh::Container::StaticVector<int, 4> v(3, -1);
+
+		EXPECT_TRUE(!v.empty());
+
+		for (const auto& i : v) {
+			EXPECT_EQ(i, -1);
+		}
+	}
+
+	{
+		Tempeh::Container::StaticVector<TestClassPOD, 4> v(3, TestClassPOD{ -1, -1, 'a' });
+
+		EXPECT_TRUE(!v.empty());
+
+		for (const auto& elem : v) {
+			EXPECT_EQ(elem.x, -1);
+			EXPECT_EQ(elem.y, -1);
+			EXPECT_EQ(elem.z, 'a');
+		}
+	}
+
+	{
+		Tempeh::Container::StaticVector<TestClass, 4> v(3, TestClass{ -1, -1, "test" });
+
+		EXPECT_TRUE(!v.empty());
+
+		for (const auto& elem : v) {
+			EXPECT_EQ(elem.x, -1);
+			EXPECT_EQ(elem.y, -1);
+			EXPECT_EQ(elem.str, "test");
+		}
+	}
+}
+
+TEST(Container_StaticArray, CopyConstructor)
+{
+	{
+		Tempeh::Container::StaticVector<int, 4> v{ 1, 2, 3 };
+		Tempeh::Container::StaticVector<int, 4> v2(v);
+
+		EXPECT_TRUE(!v2.empty());
+		EXPECT_EQ(v2[0], 1);
+		EXPECT_EQ(v2[1], 2);
+		EXPECT_EQ(v2[2], 3);
+	}
+
+	{
+		Tempeh::Container::StaticVector<TestClassPOD, 4> v{
+			{ 1, 2, 'a'},
+			{ 1, 2, 'a'},
+		};
+
+		Tempeh::Container::StaticVector<TestClassPOD, 4> v2(v);
+
+		EXPECT_TRUE(!v2.empty());
+
+		for (const auto& elem : v2) {
+			EXPECT_EQ(elem.x, 1);
+			EXPECT_EQ(elem.y, 2);
+			EXPECT_EQ(elem.z, 'a');
+		}
+	}
+
+	{
+		Tempeh::Container::StaticVector<TestClass, 4> v{
+			{ 1, 2, "test"},
+			{ 1, 2, "test"},
+		};
+
+		Tempeh::Container::StaticVector<TestClass, 4> v2(v);
+
+		EXPECT_TRUE(!v2.empty());
+
+		for (const auto& elem : v2) {
+			EXPECT_EQ(elem.x, 1);
+			EXPECT_EQ(elem.y, 2);
+			EXPECT_EQ(elem.str, "test");
+		}
+	}
+}
+
+TEST(Container_StaticArray, AssignWithIterator)
+{
+	{
+		Tempeh::Container::StaticVector<int, 4> v{ 1, 2, 3 };
+
+		// expand
+		Tempeh::Container::StaticVector<int, 4> tmp{ 2, 4, 5, 6 };
+		v.assign(tmp.begin(), tmp.end());
+
+		EXPECT_TRUE(!v.empty());
+		EXPECT_EQ(v[0], 2);
+		EXPECT_EQ(v[1], 4);
+		EXPECT_EQ(v[2], 5);
+		EXPECT_EQ(v[3], 6);
+
+		// shrink
+		Tempeh::Container::StaticVector<int, 4> tmp2{ 7, 8 };
+		v.assign(tmp2.begin(), tmp2.end());
+
+		EXPECT_TRUE(!v.empty());
+		EXPECT_EQ(v[0], 7);
+		EXPECT_EQ(v[1], 8);
+	}
+
+	{
+		Tempeh::Container::StaticVector<TestClassPOD, 4> v{
+			{ 1, 2, 'a'},
+			{ 1, 2, 'a'},
+		};
+
+		// expand
+		Tempeh::Container::StaticVector<TestClassPOD, 4> tmp{
+			{ 2, 3, 'b'},
+			{ 2, 3, 'b'},
+			{ 2, 3, 'b'},
+		};
+
+		v.assign(tmp.begin(), tmp.end());
+
+		EXPECT_TRUE(!v.empty());
+
+		for (const auto& elem : v) {
+			EXPECT_EQ(elem.x, 2);
+			EXPECT_EQ(elem.y, 3);
+			EXPECT_EQ(elem.z, 'b');
+		}
+
+		// shrink
+		Tempeh::Container::StaticVector<TestClassPOD, 4> tmp2{
+			{ 2, 3, 'c'}
+		};
+
+		v.assign(tmp2.begin(), tmp2.end());
+
+		EXPECT_TRUE(!v.empty());
+
+		for (const auto& elem : v) {
+			EXPECT_EQ(elem.x, 2);
+			EXPECT_EQ(elem.y, 3);
+			EXPECT_EQ(elem.z, 'c');
+		}
+	}
+
+	{
+		Tempeh::Container::StaticVector<TestClass, 4> v{
+			{ 1, 2, "test"},
+			{ 1, 2, "test"},
+		};
+
+		// expand
+		Tempeh::Container::StaticVector<TestClass, 4> tmp{
+			{ 1, 2, "bruh"},
+			{ 1, 2, "bruh"},
+			{ 1, 2, "bruh"},
+		};
+
+		v.assign(tmp.begin(), tmp.end());
+
+		EXPECT_TRUE(!v.empty());
+
+		for (const auto& elem : v) {
+			EXPECT_EQ(elem.x, 1);
+			EXPECT_EQ(elem.y, 2);
+			EXPECT_EQ(elem.str, "bruh");
+		}
+
+		// shrink
+		Tempeh::Container::StaticVector<TestClass, 4> tmp2{
+			{ 3, 4, "brah"},
+		};
+
+		v.assign(tmp2.begin(), tmp2.end());
+
+		EXPECT_TRUE(!v.empty());
+
+		for (const auto& elem : v) {
+			EXPECT_EQ(elem.x, 3);
+			EXPECT_EQ(elem.y, 4);
+			EXPECT_EQ(elem.str, "brah");
+		}
+	}
+}
+
+TEST(Container_StaticArray, AssignWithInitializerList)
+{
+	{
+		Tempeh::Container::StaticVector<int, 4> v{ 1, 2, 3 };
+
+		v.assign({ 2, 4, 5, 6 });
+
+		EXPECT_TRUE(!v.empty());
+		EXPECT_EQ(v[0], 2);
+		EXPECT_EQ(v[1], 4);
+		EXPECT_EQ(v[2], 5);
+		EXPECT_EQ(v[3], 6);
+
+		v.assign({ 7, 8 });
+
+		EXPECT_TRUE(!v.empty());
+		EXPECT_EQ(v[0], 7);
+		EXPECT_EQ(v[1], 8);
+	}
+
+	{
+		Tempeh::Container::StaticVector<TestClassPOD, 4> v{
+			{ 1, 2, 'a'},
+			{ 1, 2, 'a'},
+		};
+
+		v.assign({
+			{ 2, 3, 'b'},
+			{ 2, 3, 'b'},
+			{ 2, 3, 'b'},
+		});
+
+		EXPECT_TRUE(!v.empty());
+
+		for (const auto& elem : v) {
+			EXPECT_EQ(elem.x, 2);
+			EXPECT_EQ(elem.y, 3);
+			EXPECT_EQ(elem.z, 'b');
+		}
+
+		v.assign({
+			{ 2, 3, 'c'}
+		});
+
+		EXPECT_TRUE(!v.empty());
+
+		for (const auto& elem : v) {
+			EXPECT_EQ(elem.x, 2);
+			EXPECT_EQ(elem.y, 3);
+			EXPECT_EQ(elem.z, 'c');
+		}
+	}
+
+	{
+		Tempeh::Container::StaticVector<TestClass, 4> v{
+			{ 1, 2, "test"},
+			{ 1, 2, "test"},
+		};
+
+		v.assign({
+			{ 1, 2, "bruh"},
+			{ 1, 2, "bruh"},
+			{ 1, 2, "bruh"},
+		});
+
+		EXPECT_TRUE(!v.empty());
+
+		for (const auto& elem : v) {
+			EXPECT_EQ(elem.x, 1);
+			EXPECT_EQ(elem.y, 2);
+			EXPECT_EQ(elem.str, "bruh");
+		}
+
+		v.assign({
+			{ 3, 4, "brah"},
+		});
+
+		EXPECT_TRUE(!v.empty());
+
+		for (const auto& elem : v) {
+			EXPECT_EQ(elem.x, 3);
+			EXPECT_EQ(elem.y, 4);
+			EXPECT_EQ(elem.str, "brah");
+		}
+	}
+}
